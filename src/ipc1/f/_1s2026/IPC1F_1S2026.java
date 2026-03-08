d4ded6795b57eefc7a50137c69e91dff1412b6ee
Adolfo Ventura <3415025372103@ingenieria.usac.edu.gt>
2026-03-08 02:22:40 -0600

    Nueva carga

--- src/ipc1/f/_1s2026/IPC1F_1S2026.java
+++ src/ipc1/f/_1s2026/IPC1F_1S2026.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ipc1.f._1s2026;
 
 /**
@@ -10,11 +7,8 @@ package ipc1.f._1s2026;
  */
 public class IPC1F_1S2026 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        System.out.println( "Hola mundo " );
     
+}
 }
